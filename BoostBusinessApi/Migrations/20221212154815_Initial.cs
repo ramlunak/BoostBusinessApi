ee26ab3f595f13646cf6aaed7d5542e1098d0fc3
Royber Arias <royber2020@gmail.com>
2022-12-20 18:31:28 -0300

    sqlite

--- BoostBusinessApi/Migrations/20221212154815_Initial.cs
+++ BoostBusinessApi/Migrations/20221212154815_Initial.cs
@@ -10,19 +10,20 @@ namespace BoostBusinessApi.Migrations
         /// <inheritdoc />'12
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Users",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Users", x => x.Id);
-                });
+            //IF SQL SERVER
+            //migrationBuilder.CreateTable(
+            //    name: "Users",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Users", x => x.Id);
+            //    });
         }
 
         /// <inheritdoc />
