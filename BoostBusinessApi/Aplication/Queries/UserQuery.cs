c9f19aec0fa4a95597b4e9000f31ace839032e67
Royber Arias <royber2020@gmail.com>
2022-12-23 10:58:02 -0300

    UserQuery

--- BoostBusinessApi/Aplication/Queries/UserQuery.cs
+++ BoostBusinessApi/Aplication/Queries/UserQuery.cs
@@ -5,5 +5,8 @@ namespace BoostBusinessApi.Aplication.Queries
 {
     public class UserQuery : IRequest<IEnumerable<UserViewModel>>
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
